a4024cb68bf91e933239116ff9e215e21521d415
weizili.laptop <zieckey@gmail.com>
2015-11-14 11:26:30 +0800

    add comments

--- tmail/tmail_test.go
+++ tmail/tmail_test.go
@@ -4,5 +4,6 @@ import (
 )
 
 func TestFindAll(t *testing.T) {
-	SendMail("hello, 关于mail问题", "Hi, 这个问题我同意。", "weizili@360.cn", "zieckey@163.com")
+	SendMail("hello, 关于mail问题", "Hi, 这个问题我同意。", "weizili@360.cn", "zieckey@163.com") //TODO 收到的邮件有乱码
+	SendMail("about the student", "I agree.", "weizili@360.cn", "zieckey@163.com")
 }
